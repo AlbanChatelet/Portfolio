446a1598b22ffc984b54b82960bff713f292e6b6
AlbanChatelet <chateletalban20@gmail.com>
2024-11-18 17:09:27 +0100

    finitions

--- src/pages/projets/[id].vue
+++ src/pages/projets/[id].vue
@@ -115,11 +115,11 @@
       </div>
     </div>
 
-    <div class="md:flex md:gap-x-8 ml-auto">
-      <div class="md:w-[310px] md:h-[70px] w-[210px] rounded-[5px] bg-interactions flex items-center justify-center project-button mt-10 mx-auto">
+    <div class="md:flex">
+      <div class="md:w-[310px] md:h-[70px] w-[210px] rounded-[5px] bg-interactions flex items-center justify-center project-button mt-10 mx-auto md:mr-8">
         <p class="text-white font-handjet text-3xl md:text-4xl">Lien vers le site</p>
       </div>
-      <div class="md:w-[310px] md:h-[70px] w-[210px] rounded-[5px] border-solid border-2 border-interactions flex items-center justify-center project-button mt-4 md:mt-10 mx-auto">
+      <div class="md:w-[310px] md:h-[70px] w-[210px] rounded-[5px] border-solid border-2 border-interactions flex items-center justify-center project-button mt-4 md:mt-10 mx-auto md:mr-24">
         <p class="text-interactions font-handjet text-3xl md:text-4xl">Retour à l'accueil</p>
       </div>
     </div>
