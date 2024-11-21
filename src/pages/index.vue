10cb61c16a6f84bc11cf5ee618cb38c4f7b1d1ee
AlbanChatelet <chateletalban20@gmail.com>
2024-11-21 10:14:17 +0100

    responsive

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -371,10 +371,10 @@ function scrollToTop() {
 
 <button
   @click="scrollToTop"
-  class="fixed bottom-16 right-16 py-2 px-4 rounded-full transition-transform transform hover:scale-105"
+  class="fixed bottom-6 right-2 md:bottom-16 md:right-16  rounded-full transition-transform transform hover:scale-105"
   aria-label="Remonter en haut"
 >
-  <scrollReset />
+  <scrollReset class="h-[59px] w-[60px]" />
 </button>
 
 
